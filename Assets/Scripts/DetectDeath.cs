81039a5f29068ebe366c6271d484ee37da2b7206
RealWigglyOrange <realwigglyorange@gmail.com>
2025-07-31 15:19:22 -0400

    Code
    updated to add the sprites to the movement and death
    
    NOTE: death timer no longer resets; i might have just been impatient

--- Assets/Scripts/DetectDeath.cs
+++ Assets/Scripts/DetectDeath.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class DetectDeath : MonoBehaviour
 {
+    [SerializeField] private PlayerController player;
     [SerializeField] private GameMode GM;
     [SerializeField] private Progress Progress;
+
+    private float localTimer = 30;
 
     // Update is called once per frame
     void Update()
     {
         if (GM.timer >= 180)
         {
-            Debug.Log("You Died");
-            GM.timer = 0;
-            Progress.clear();
+            if (player.spriteRenderer.sprite == player.fhappy || player.spriteRenderer.sprite == player.fangry)
+            {
+                player.spriteRenderer.sprite = player.fdead;
+            } else if (player.spriteRenderer.sprite == player.bhappy || player.spriteRenderer.sprite == player.bangry)
+            {
+                player.spriteRenderer.sprite = player.bdead;
+            }
+
+            localTimer -= Time.deltaTime;
+
+            if (localTimer <= 0)
+            {
+                GM.timer = 0;
+                Progress.clear();
+                player.resetPlayer();
+                localTimer = 30;
+            }
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public SpriteRenderer spriteRenderer;
+    public Sprite fhappy;
+    public Sprite bhappy;
+    public Sprite fdead;
+    public Sprite bdead;
+    public Sprite fangry;
+    public Sprite bangry;
+
     private float Vin;
     private float Hin;
+    public bool interacting;
 
     public float speed;
 
@@ -20,7 +29,22 @@ public class PlayerController : MonoBehaviour
     {
         Vin = Input.GetAxis("Vertical");
         Hin = Input.GetAxis("Horizontal");
+        interacting = Input.GetKey(KeyCode.F);
 
         transform.position += Vector3.Normalize(new Vector3(Hin, Vin)) * speed * Time.deltaTime;
+
+        if (Vin > 0)
+        {
+            spriteRenderer.sprite = bhappy;
+        } else if (Vin < 0)
+        {
+            spriteRenderer.sprite = fhappy;
+        }
+    }
+
+    public void resetPlayer()
+    {
+        // reset position, sprite
+        spriteRenderer.sprite = fhappy;
     }
 }
